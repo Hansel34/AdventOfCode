59c1c1271cff176e3971705ab171c90baffad1aa
hansel34 <qiu_hansel@hotmail.com>
2018-12-02 19:00:48 -0700

    day 2

--- day2.py
+++ day2.py
@@ -1,21 +1,41 @@
-def func():
-    total = 0
-    dic = {}
-    dic[total] = True
+from collections import Counter
+def func1():
+    twos = 0
+    threes = 0
     inputs = []
     while True:
         line = input()
         if line == "end":
             break
         inputs.append(line)
+    
+    for line in inputs:
+        line = Counter(line)
+        for key,val in line.items():
+            if val == 2:
+                twos+=1
+                break
+        for key,val in line.items():
+            if val == 3:
+                threes+=1
+                break
+    print(twos*threes)
 
+def func2():
+    inputs = []
     while True:
-        for line in inputs:
-            if line[0] == '+':
-                total += int(line[1:])
-            else:
-                total -= int(line[1:])
-            if total in dic:
-                return total
-            dic[total] = True
-print(func())
+        line = input()
+        if line == "end":
+            break
+        inputs.append(line)
+    
+    for line in inputs:
+        for line2 in inputs:
+            differences = 0
+            for i in range(len(line)):
+                if line[i]!=line2[i]:
+                    differences+=1
+            if differences == 1:
+                return line+"  "+line2
+
+print(func2())
